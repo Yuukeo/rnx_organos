b10558709b2c36387add37477d05c821ab0f449a
keiki <sexplosive42@gmail.com>
2020-03-12 00:01:52 +0700

    Update __resource.lua

--- __resource.lua
+++ __resource.lua
@@ -1,18 +1,21 @@
+fx_version 'bodacious'
+
+game 'gta5'
+
 description 'RNX Organ FIX'
 
 version '2.0'
 
 server_scripts {
   '@es_extended/locale.lua',
-	'locales/es.lua',
+  'locales/es.lua',
   'server/main.lua',
   'config.lua'
-
 }
+
 client_scripts {
   '@es_extended/locale.lua',
-	'locales/es.lua',
+  'locales/es.lua',
   'config.lua',
   'client/main.lua'
-
 }
