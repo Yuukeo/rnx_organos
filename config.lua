f35df7e77066495f086288e34b50ed6cb4066c84
keiki <sexplosive42@gmail.com>
2020-03-12 00:00:15 +0700

    minor cleanup

--- config.lua
+++ config.lua
@@ -1,8 +1,8 @@
-Config              = {}
-Config.MarkerType   = 1
+Config = {}
+Config.MarkerType = 1
 Config.DrawDistance = 40.0
-Config.ZoneSize     = {x = 2.0, y = 2.0, z = 2.0}
-Config.MarkerColor  = {r = 255, g = 0, b = 0}
+Config.ZoneSize = {x = 2.0, y = 2.0, z = 2.0}
+Config.MarkerColor = {r = 255, g = 0, b = 0}
 Config.RequiredCopsCerebro = 0
 Config.RequiredCopsCorazon = 0
 Config.RequiredCopsMedulas = 0
@@ -11,11 +11,11 @@ Config.RequiredCopsBody = 1
 Config.Locale = 'es'
 
 Config.Zones = {
-    CerebroFarm =     {x = -1673.5,  y = 370.99, z = 84.12}, -- Cerebro
-    CorazonFarm =       {x = -1146.88,  y = -1515.55, z = 9.63}, -- Corazon
-    MedulasFarm =      {x = -592.67,  y = -1618.0, z = 32.02}, -- Medula
-    InstestinosFarm =    {x = 1754.76,  y = -1649.04, z = 111.65}, -- Intestino
-    HuesosFarm =          {x = -463.76,  y = -2284.4, z = 6.61}, -- Huesos
-    CoserCuerpos =  {x = 1343.14, y = 4390.03, z = 43.34}, -- Tratamiento de organos
-    VentaCuerpos =      {x = 1207.21,  y = -3121.87, z = 4.54} -- Venta de cuerpos
+    CerebroFarm = {x = -1673.5, y = 370.99, z = 84.12},         -- Cerebro
+    CorazonFarm = {x = -1146.88, y = -1515.55, z = 9.63},       -- Corazon
+    MedulasFarm = {x = -592.67, y = -1618.0, z = 32.02},        -- Medula
+    InstestinosFarm = {x = 1754.76, y = -1649.04, z = 111.65},  -- Intestino
+    HuesosFarm = {x = -463.76, y = -2284.4, z = 6.61},          -- Huesos
+    CoserCuerpos = {x = 1343.14, y = 4390.03, z = 43.34},       -- Tratamiento de organos
+    VentaCuerpos = {x = 1207.21, y = -3121.87, z = 4.54}        -- Venta de cuerpos
 }
